ba618dba5f8da1428819e713b31b9431bbb5c80a
leQwang <s3912105@rmit.edu.vn>
2023-08-24 00:42:53 +0700

    Update seller.router.js

--- src/router/seller.router.js
+++ src/router/seller.router.js
@@ -4,5 +4,4 @@ import { getAllCategoryTreeAPI } from "../api/category/get-category-tree.api.js"
 
 
 const router = express.Router();
-router.get("/category", authenticateUser, getAllCategoryTreeAPI);
 export const sellerRouter = router;
