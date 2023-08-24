fcaffe7be2198228b3ac7135771b1a50a04b1d52
leQwang <s3912105@rmit.edu.vn>
2023-08-25 00:26:24 +0700

    add create product api

---
+++ src/api/seller/create-product.api.js
@@ -0,0 +1,43 @@
+import { ROLE } from "../../constants/index.js";
+import { productSchema } from "../../repository/Schemas/product.schema.js";
+import { Seller } from "../../models/Seller.js";
+import { Product } from "../../models/Product.js";
+
+
+export const createProductAPI = async (req, res) => {
+  const authUser = req.authUser;
+  const { role: isAuthRole } = authUser;
+  const { title, price, description, categories, avatar } = req.body;
+
+  if (isAuthRole !== ROLE.SELLER) {
+    return res.status(401).json({
+      message: "Unauthorized current user is not a seller",
+    });
+  }
+
+  try {
+    const product = new Product(productSchema, { title, price, description, categories, avatar });
+    await product.insertProductToDatabase();
+    return res.status(200).json({
+      message: "Product created",
+      product: product,
+    });
+  } catch (err) {
+    console.log(err);
+    if (err.errors) {
+      const { title, price, description, categories, avatar } = err.errors;
+      return res.status(400).json({
+        message: "Product not created here",
+        title: title?.message,
+        price: price?.message,
+        description: description?.message,
+        categories: categories?.message,
+        avatar: avatar?.message,
+      });
+    }
+    res.status(500).json({
+      message: "Product not created",
+      error: "Internal server error",
+    });
+  }
+};
--- src/router/seller.router.js
+++ src/router/seller.router.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { authenticateUser } from "../middleware/auth.middleware.js";
-import { getAllCategoryTreeAPI } from "../api/category/get-category-tree.api.js";
+import { createProductAPI } from "../api/seller/create-product.api.js";
 
 
 const router = express.Router();
+router.post("/createProduct", authenticateUser, createProductAPI);
 export const sellerRouter = router;
