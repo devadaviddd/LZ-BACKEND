192409128f2a343341cf7598353d0aa718ad385f
devadaviddd <s3927112@rmit.edu.vn>
2023-09-10 13:31:00 +0700

    change regex for phone

--- src/di/index.js
+++ src/di/index.js
@@ -1,13 +1,3 @@
 import { Database } from "../repository/database.js";
 
 export const database = new Database();
-
-
-// Product.getAllProduct(database);
-
-
-
-// class Product 
-
-   // static getAllProduct(database) { database.getAllProdcut() }
-  
