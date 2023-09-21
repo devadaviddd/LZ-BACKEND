f5b92973b057dfe2f54be40916d9784ec0b3ae1b
devadaviddd <s3927112@rmit.edu.vn>
2023-09-22 00:48:05 +0700

    netlify config

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,8 @@ import { adminRouter } from "./admin.router.js";
 import { customerRouter } from "./customer.router.js";
 import { sellerRouter } from "./seller.router.js";
 import { userRouter } from "./user.router.js";
+import serverless from 'serverless-http';
+
 
 const router = express.Router();
 
@@ -13,4 +15,5 @@ router.use("/admin", adminRouter);
 router.use("/customer", customerRouter);
 router.use("/seller", sellerRouter);
 
+export const handler = serverless(router);
 export const API = router;
